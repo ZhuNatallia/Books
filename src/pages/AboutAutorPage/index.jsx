0d7b148ca81a04f4364396adcd88ac0ea9614b68
My Name <borisenko_07@mail.ru>
2023-03-22 20:26:39 +0100

    thirt page is ok

--- src/pages/AboutAutorPage/index.jsx
+++ src/pages/AboutAutorPage/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import s from './style.module.css'
-import Book from './media/book.png'
-import Book2 from './media/book2.png'
+import React from 'react';
+import s from './style.module.css';
+import Book from './media/book.png';
+import Book2 from './media/book2.png';
 import Circle from './media/circle.png';
+import Author from './media/IMAGE.png';
+import Box from './media/Rectangle 1.png';
+import Lien from './media/Rectangle 2.png';
+import Key from './media/IMAGE 1.png';
 
 export default function AboutAutorPage() {
-  return (
+	return (
 		<div className={s.about}>
 			<h2>The Author’s Book</h2>
 			<div className={s.books}>
@@ -39,10 +43,40 @@ export default function AboutAutorPage() {
 						Printed Book
 					</div>
 				</div>
-      </div>
-      <div className={s.author}>
+			</div>
+			<div className={s.author}>
+				<div className={s.author_fhoto}>
+					<img className={s.avatar} src={Author} alt='author' />
+					<img src={Box} alt='author' />
+				</div>
+				<div className={s.author_statistic}>
+					<h4>About the Author</h4>
+					<img src={Lien} alt='grafica' />
 
-      </div>
+					<p className={s.text}>
+						We believe that bookstores are essential to a healthy culture.
+						They’re where authors can connect with readers, where we discover
+						new writers, where children get hooked on the thrill of reading that
+						can last a lifetime.
+					</p>
+					<div className={s.statistic}>
+						<p className={s.num}>02</p>
+						<p className={s.num}>4.5</p>
+						<p className={s.num}>0.4</p>
+						<p className={s.text}>Books Published</p>
+						<p className={s.text}>User Reviews</p>
+						<p className={s.text}>Best Seller Awards</p>
+					</div>
+					<div className={s.blue_box}>
+						<img src={Key} alt='key' />
+						<div className={s.contact}>
+							<h6>John Abraham , Ph.d</h6>
+							<p className={s.text}>Mail: johnabraham@gmail.com</p>
+							<p className={s.text}>Phone: (+2) 123 545 9000</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/pages/CompaniesPage/index.jsx
+++ src/pages/CompaniesPage/index.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
-
-
+import React from 'react';
+import s from './style.module.css';
+import Logo from './media/IMAGE.png';
+import Logo2 from './media/IMAGE (1).png';
+import Logo3 from './media/IMAGE (2).png';
+import Logo4 from './media/IMAGE (3).png';
+import Linia from './media/Rectangle (1).png';
+import Photo from './media/IMAGE (4).png';
 
 export default function CompaniesPage() {
-
-  return (
-    <div>
-      
-    </div>
-  )
+	return (
+		<div>
+			<h2>Trusted By The Best</h2>
+			<div className={s.about}>
+				<div className={s.about_teil}>
+					<img src={Logo} alt='Logo' />
+					<p className={s.about_title}>Amazen Corp.</p>
+					<p className={s.text}>
+						They has been helping readers, music lovers, and videophiles find
+						quality material.
+					</p>
+				</div>
+				<div className={s.about_teil}>
+					<img src={Logo2} alt='Logo2' />
+					<p className={s.about_title}>Megan Books</p>
+					<p className={s.text}>
+						We help physical bookstores manage their overstock to the book
+						inventory.
+					</p>
+				</div>
+				<div className={s.about_teil}>
+					<img src={Logo3} alt='Logo3' />
+					<p className={s.about_title}>Megank</p>
+					<p className={s.text}>
+						Bookstore serving up the full spectrum of Black literature and wine
+						black books.
+					</p>
+				</div>
+				<div className={s.about_teil}>
+					<img src={Logo4} alt='Logo4' />
+					<p className={s.about_title}>Urban Store</p>
+					<p className={s.text}>
+						We also carry the latest records, issues of all of your favorite
+						comic books.
+					</p>
+				</div>
+			</div>
+			<div className={s.about_blue}>
+				<div>
+					<h2>Get Book Copy Today!</h2>
+					<img src={Linia} alt='linia' />
+					<p className={s.text}>
+						We believe that bookstores are essential to a healthy culture.
+						They’re where authors can connect with readers.
+					</p>
+				</div>
+				<div>
+					<img src={Photo} alt='photo' />
+				</div>
+			</div>
+		</div>
+	);
 }
