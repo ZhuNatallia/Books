3e4b6d2850558f9dce1acb3be2203ef5914b331d
My Name <borisenko_07@mail.ru>
2023-03-23 18:17:30 +0100

    ist fertig

--- src/pages/ArticlesPage/index.jsx
+++ src/pages/ArticlesPage/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import s from './style.module.css';
 import Photo from './media/IMAGE.png';
 import Rectangle from './media/Rectangle (1).png';
+import Photo1 from './media/IMAGE (1).png';
+import Photo2 from './media/IMAGE (2).png';
+import Photo3 from './media/IMAGE (3).png';
 
 export default function ArticlesPage() {
 	return (
@@ -37,6 +40,35 @@ export default function ArticlesPage() {
 				<div className={s.photo_box}>
 					<img className={s.photo} src={Photo} alt='Photo' />
 					<img className={s.rectangle} src={Rectangle} alt='Rectangle' />
+				</div>
+			</div>
+			<div className={s.articles_gray}>
+				<h2>Articles & Resources</h2>
+				<div className={s.wrapper_card}>
+					<div className={s.card}>
+						<img src={Photo1} alt='avatar' />
+						<h5>Significant reading has more info number</h5>
+						<p className={s.text}>
+							Override the digital divide with additional clickthroughs from
+							DevOps for real-time schemas.
+						</p>
+					</div>
+					<div className={s.card}>
+						<img src={Photo2} alt='avatar' />
+						<h5>Many variations of pass majority have suffered</h5>
+						<p className={s.text}>
+							Capitalize on low-hanging fruit to identify a ballpark value-added
+							activity to beta test.
+						</p>
+					</div>
+					<div className={s.card}>
+						<img src={Photo3} alt='avatar' />
+						<h5>Words which don’t look even slightly believable</h5>
+						<p className={s.text}>
+							Podcasting operational change management inside of workflows to
+							establish a framework.
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
